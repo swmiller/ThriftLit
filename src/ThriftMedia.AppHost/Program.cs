b94a02326e06299abc6cda9143208fa5c9a23a19
Stephen Miller <smiller@crowninformationservices.com>
2025-07-01 15:24:22 -0500

    Added public facing web app and api.

--- src/ThriftMedia.AppHost/Program.cs
+++ src/ThriftMedia.AppHost/Program.cs
@@ -1,3 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.ThriftMedia_Web>("thriftmediaweb");
+
+builder.AddProject<Projects.ThriftMedia_Api>("thriftmediaapi");
+
 builder.Build().Run();
+
