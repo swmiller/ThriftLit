c03889719a4cadbd1de0846c957aca3def3926ad
Stephen Miller <smiller@crowninformationservices.com>
2025-07-16 10:49:53 -0500

    Added "media" tables.

---
+++ src/ThriftMedia.Database/Indexes/MediaItems.FullTextIndexes.sql
@@ -0,0 +1,11 @@
+﻿
+--CREATE FULLTEXT CATALOG ftCatalog AS DEFAULT;
+--GO
+
+--CREATE FULLTEXT INDEX ON [media].[MediaItems]
+--(
+--    [OcrPayloadJson] LANGUAGE 1033
+--)
+--KEY INDEX PK_MediaItems;
+--GO
+
---
+++ src/ThriftMedia.Database/Tables/MediaItems.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [media].[MediaItems] (
+    [MediaId]        INT            IDENTITY (1, 1) NOT NULL,
+    [StoreOwnerId]   INT            NOT NULL,
+    [MediaType]      NVARCHAR(50)   NOT NULL,
+    [ImageUrl]       NVARCHAR(500)  NOT NULL,
+    [OcrPayloadJson] NVARCHAR(MAX)  NOT NULL,
+    [CreatedBy]      NVARCHAR(100)  NOT NULL,
+    [CreatedAt]      DATETIME       NOT NULL DEFAULT (GETDATE()),
+    [UpdatedBy]      NVARCHAR(100)  NULL,
+    [UpdatedAt]      DATETIME       NULL, 
+    CONSTRAINT PK_MediaItems PRIMARY KEY ([MediaId]),
+    CONSTRAINT [FK_MediaItems_StoreOwner] FOREIGN KEY ([StoreOwnerId]) REFERENCES [media].[StoreOwner]([Id])
+);
