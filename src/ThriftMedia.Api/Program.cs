75fbe1377abb586b23e4e9c05fea3f038e539e33
Stephen Miller <smiller@crowninformationservices.com>
2025-08-06 10:44:02 -0500

    refactor: remove unused DbContext registration and related using directive

--- src/ThriftMedia.Api/Program.cs
+++ src/ThriftMedia.Api/Program.cs
@@ -1,16 +1,10 @@
 
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Inject ThriftMediaDb connection string into ThriftMediaDbContext
-builder.Services.AddDbContext<ThriftMedia.Data.Models.ThriftMediaDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ThriftMediaDb")));
 
 var app = builder.Build();
 
